d506ea3d3defa31e2771ce86af310b62d045ea41
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2024-06-07 18:37:24 -0700

    stub out models for game and poker

---
+++ src/app/game/game-models.ts
@@ -0,0 +1,71 @@
+import { User } from '../user/user-models';
+
+export type GameStatus = 'ACTIVE' | 'COMPLETED' | 'PAUSED' | 'SCHEDULED';
+export type GameFormat = 'CASH' | 'TOURNAMENT';
+export type GameType = 'TEXAS_HOLDEM';
+
+export type PlayerStatus =
+  | 'ACTIVE'
+  | 'OUT'
+  | 'AWAY'
+  | 'REGISTERED'
+  | 'OBSERVER';
+
+export interface GameCriteria {
+  name: string;
+  status: GameStatus;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Player {
+  user: User;
+  confirmed: boolean;
+  status: PlayerStatus;
+  chipCount: number;
+}
+export interface Table {}
+
+export interface Game {
+  id: string;
+  name: string;
+  status: GameStatus;
+  format: GameFormat;
+  type: GameType;
+  startTimestamp: Date;
+  endTimestamp: Date | undefined;
+  owner: User;
+  players: Map<string, Player>;
+  tables: Table[];
+}
+
+export interface CashGameDetails {
+  id: string;
+  name: string;
+  gameType: GameType;
+  startTimestamp: Date;
+  buyInAmount: number;
+  owner: User;
+  smallBlind: number;
+  bigBlind: number;
+  numberOfPlayers: number;
+}
+
+export class CashGame implements Game {
+  public readonly format: GameFormat = 'CASH';
+
+  constructor(
+    public id: string,
+    public name: string,
+    public status: GameStatus,
+    public type: GameType,
+    public owner: User,
+    public startTimestamp: Date,
+    public endTimestamp: Date | undefined,
+    public players: Map<string, Player>,
+    public tables: Table[],
+    public smallBlind: number,
+    public bigBlind: number,
+    public buyInAmount: number
+  ) {}
+}
---
+++ src/app/poker/poker-models.ts
@@ -0,0 +1,27 @@
+export enum CardValue {
+  TWO = '2',
+  THREE = '3',
+  FOUR = '4',
+  FIVE = '5',
+  SIX = '6',
+  SEVEN = '7',
+  EIGHT = '8',
+  NINE = '9',
+  TEN = '10',
+  JACK = 'J',
+  QUEEN = 'Q',
+  KING = 'K',
+  ACE = 'A',
+}
+
+export enum CardSuit {
+  HEARTS = 'H',
+  DIAMONDS = 'D',
+  CLUBS = 'C',
+  SPADES = 'S',
+}
+
+export interface Card {
+  value: CardValue;
+  suit: CardSuit;
+}
