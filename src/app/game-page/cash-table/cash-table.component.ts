29aea76df43b3d83d3deea50777abfa206d6f487
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2024-07-07 14:28:43 -0700

    render players around the table

--- src/app/game-page/cash-table/cash-table.component.ts
+++ src/app/game-page/cash-table/cash-table.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../poker/card/card.component';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-cash-table',
   standalone: true,
   templateUrl: './cash-table.component.html',
   styleUrl: './cash-table.component.css',
-  imports: [CardComponent],
+  imports: [CardComponent, PlayerComponent],
 })
 export class CashTableComponent {}
